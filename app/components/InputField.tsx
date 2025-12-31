a184477751a867b98aefa626de4722c0d1292fd4
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2025-12-31 12:46:50 +0530

    extend input field component for select

--- app/components/InputField.tsx
+++ app/components/InputField.tsx
@@ -5,11 +5,17 @@ interface InputFieldProps {
   name: string;
   type?: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  disabled?: boolean;
   required?: boolean;
   placeholder?: string;
   pattern?: string;
   className?: string;
+  options?: readonly { readonly value: string; readonly label: string }[];
+  isSelect?: boolean;
+  addSelectOther?: boolean;
 }
 
 export default function InputField({
@@ -18,10 +24,14 @@ export default function InputField({
   type = "text",
   value,
   onChange,
+  disabled = false,
   required = false,
   placeholder,
   pattern,
   className = "",
+  options = [],
+  isSelect = false,
+  addSelectOther = false,
 }: InputFieldProps) {
   return (
     <div className="input-group">
@@ -29,16 +39,45 @@ export default function InputField({
         {label}
         {required && <span className="required_field"> *</span>}
       </label>
-      <input
-        className={`input--style-4 ${className}`.trim()}
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        required={required}
-        placeholder={placeholder}
-        pattern={pattern}
-      />
+      {isSelect ? (
+        <div className="rs-select2 js-select-simple select--no-search">
+          <select
+            className={`input--style-4 ${className}`.trim()}
+            name={name}
+            value={value}
+            onChange={onChange}
+            required={required}
+            disabled={disabled}
+          >
+            <option value="" disabled>
+              {placeholder || "Choose option"}
+            </option>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+            {addSelectOther && (
+              <option key="Other" value="Other">
+                Other
+              </option>
+            )}
+          </select>
+          <div className="select-dropdown"></div>
+        </div>
+      ) : (
+        <input
+          className={`input--style-4 ${className}`.trim()}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          required={required}
+          placeholder={placeholder}
+          pattern={pattern}
+          disabled={disabled}
+        />
+      )}
     </div>
   );
 }
